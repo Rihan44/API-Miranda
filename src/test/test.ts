dc8adff50e50bb3aada0d53c8b717b95ccda63f2
Rihan44 <asmuela_bustamante@hotmail.es>
2023-10-26 13:28:41 +0200

    serverless removed from repo

--- src/test/test.ts
+++ src/test/test.ts
@@ -9,3 +9,16 @@ describe('POST login ', () => {
 
     });
 });
+
+/* describe('POST Create User Wallet', () => {
+    it('should create wallet for the user', () => {
+        request(app)
+        .post('123456/wallet')
+        .send({})
+        .expect(201)
+        .then((res) => {
+         expect(res.headers.location).to.be.eql('123456/wallet');
+         // more validations can be added here as required
+    });
+ });
+}); */
