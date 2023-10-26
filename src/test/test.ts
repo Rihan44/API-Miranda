5d7ceb83f4acc70f2218ad923a57e60e65a5666e
Rihan44 <asmuela_bustamante@hotmail.es>
2023-10-26 13:24:30 +0200

    test folder and test file created

---
+++ src/test/test.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+const chai = require('chai');
+const request = require('supertest');
+
+const app = express();
+
+describe('POST login ', () => {
+    it('should login', () => {
+
+    });
+});
