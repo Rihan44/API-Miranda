eb208a0d49344d6b1e09ec38d7e3f5e47543479d
Rihan44 <asmuela_bustamante@hotmail.es>
2023-11-09 12:10:12 +0100

    [Update] booking service update with left join

--- dist/services/bookings.js
+++ dist/services/bookings.js
@@ -3,12 +3,34 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.bookingService = void 0;
 const connection_1 = require("../utils/connection");
 async function getAllBookings() {
-    const query = "SELECT * FROM bookings";
+    const query = `SELECT
+        r.*, b.*,
+        GROUP_CONCAT(DISTINCT r.room_type) AS room_type,
+        GROUP_CONCAT(DISTINCT r.room_number) AS room_number,
+        GROUP_CONCAT(DISTINCT r.description) AS description,
+        GROUP_CONCAT(DISTINCT r.offer_price) AS offer_price,
+        COALESCE(GROUP_CONCAT(DISTINCT a.amenity_name), 'Free Wifi, TV') AS all_amenities
+        FROM bookings b
+        LEFT JOIN rooms r ON r.id = b.room_id
+        LEFT JOIN amenity_to_room atr ON r.id = atr.room_id
+        LEFT JOIN amenities a ON atr.amenity_id = a.id AND atr.room_id = r.id
+    GROUP BY b.id`;
     const row = await (0, connection_1.queryExecuter)(query);
     return row;
 }
 async function getById(id) {
-    const query = 'SELECT * FROM bookings WHERE id = ?';
+    const query = `SELECT
+        r.*, b.*,
+        GROUP_CONCAT(DISTINCT r.room_type) AS room_type,
+        GROUP_CONCAT(DISTINCT r.room_number) AS room_number,
+        GROUP_CONCAT(DISTINCT r.description) AS description,
+        GROUP_CONCAT(DISTINCT r.offer_price) AS offer_price,
+        COALESCE(GROUP_CONCAT(DISTINCT a.amenity_name), 'Free Wifi, TV') AS all_amenities
+        FROM bookings b
+        LEFT JOIN rooms r ON r.id = b.room_id
+        LEFT JOIN amenity_to_room atr ON r.id = atr.room_id
+        LEFT JOIN amenities a ON atr.amenity_id = a.id AND atr.room_id = r.id WHERE b.id = ?
+    GROUP BY b.id`;
     const row = await (0, connection_1.queryExecuter)(query, [id]);
     return row;
 }
@@ -33,7 +55,7 @@ async function updateBooking(id, updateData) {
     const order_date = new Date(updateData.order_date);
     const check_in = new Date(updateData.check_in);
     const check_out = new Date(updateData.check_out);
-    const query = 'UPDATE bookings SET guest = ?, phone_number = ?,order_date = ?, check_in = ?, check_out = ?, special_request = ?, price = ? WHERE id = ?';
+    const query = 'UPDATE bookings SET guest = ?, phone_number = ?,order_date = ?, check_in = ?, check_out = ?, special_request = ?, price = ?, room_id = ? WHERE id = ?';
     const dataUpdated = [
         updateData.guest,
         updateData.phone_number,
@@ -42,6 +64,7 @@ async function updateBooking(id, updateData) {
         check_out,
         updateData.special_request,
         updateData.price,
+        updateData.room_id,
         id
     ];
     await (0, connection_1.queryExecuter)(query, dataUpdated);
--- src/services/bookings.ts
+++ src/services/bookings.ts
@@ -2,13 +2,37 @@ import { IBookings } from "../interfaces/Ibookings";
 import {queryExecuter} from "../utils/connection";
 
 async function getAllBookings() {
-    const query = "SELECT * FROM bookings";
+    const query = 
+    `SELECT
+        r.*, b.*,
+        GROUP_CONCAT(DISTINCT r.room_type) AS room_type,
+        GROUP_CONCAT(DISTINCT r.room_number) AS room_number,
+        GROUP_CONCAT(DISTINCT r.description) AS description,
+        GROUP_CONCAT(DISTINCT r.offer_price) AS offer_price,
+        COALESCE(GROUP_CONCAT(DISTINCT a.amenity_name), 'Free Wifi, TV') AS all_amenities
+        FROM bookings b
+        LEFT JOIN rooms r ON r.id = b.room_id
+        LEFT JOIN amenity_to_room atr ON r.id = atr.room_id
+        LEFT JOIN amenities a ON atr.amenity_id = a.id AND atr.room_id = r.id
+    GROUP BY b.id`;
+
     const row = await queryExecuter(query);
     return row;
 }
 
 async function getById(id: string) {
-    const query = 'SELECT * FROM bookings WHERE id = ?';
+    const query = `SELECT
+        r.*, b.*,
+        GROUP_CONCAT(DISTINCT r.room_type) AS room_type,
+        GROUP_CONCAT(DISTINCT r.room_number) AS room_number,
+        GROUP_CONCAT(DISTINCT r.description) AS description,
+        GROUP_CONCAT(DISTINCT r.offer_price) AS offer_price,
+        COALESCE(GROUP_CONCAT(DISTINCT a.amenity_name), 'Free Wifi, TV') AS all_amenities
+        FROM bookings b
+        LEFT JOIN rooms r ON r.id = b.room_id
+        LEFT JOIN amenity_to_room atr ON r.id = atr.room_id
+        LEFT JOIN amenities a ON atr.amenity_id = a.id AND atr.room_id = r.id WHERE b.id = ?
+    GROUP BY b.id`;
     const row = await queryExecuter(query, [id]);
     return row;
 }
@@ -39,7 +63,7 @@ async function updateBooking(id: string, updateData: IBookings) {
     const check_in = new Date(updateData.check_in);
     const check_out = new Date(updateData.check_out);
 
-    const query = 'UPDATE bookings SET guest = ?, phone_number = ?,order_date = ?, check_in = ?, check_out = ?, special_request = ?, price = ? WHERE id = ?';
+    const query = 'UPDATE bookings SET guest = ?, phone_number = ?,order_date = ?, check_in = ?, check_out = ?, special_request = ?, price = ?, room_id = ? WHERE id = ?';
 
     const dataUpdated = [
         updateData.guest,
@@ -49,6 +73,7 @@ async function updateBooking(id: string, updateData: IBookings) {
         check_out,
         updateData.special_request,
         updateData.price,
+        updateData.room_id,
         id
     ];
 
